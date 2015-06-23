0d50b013a94b25f0fb3f1f164a17f42d42abd4e2
Martin Bilski <martinb@criticue.com>
2015-06-23 12:48:28 +0200

    Cursive REPL support.

--- project.clj
+++ project.clj
@@ -1,14 +1,19 @@
+;https://github.com/bhauman/lein-figwheel/wiki/Running-figwheel-in-a-Cursive-Clojure-REPL
 (defproject decl-ui "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                           [org.clojure/clojurescript "0.0-3308"]]
+                           [org.clojure/clojurescript "0.0-3308"]
+                           [figwheel-sidecar "0.3.3"]
+                           [figwheel "0.3.3"]]
 
             :plugins [[lein-cljsbuild "1.0.6"]
                       [lein-doo "0.1.1-SNAPSHOT"]
                       [lein-figwheel "0.3.3"]]
+
+            :figwheel     {:nrepl-port 7888}
 
             :cljsbuild {:builds [{:id           "app"
                                   :source-paths ["src/"]
